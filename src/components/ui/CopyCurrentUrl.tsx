b95be022405f6211253a1825eb4212591934c127
orlein <pcj9024@gmail.com>
2024-01-27 22:20:10 +0900

    fix window undefined error

--- src/components/ui/CopyCurrentUrl.tsx
+++ src/components/ui/CopyCurrentUrl.tsx
@@ -3,5 +3,8 @@
 import CopyText from '@/src/components/ui/CopyText';
 
 export default function CopyCurrentUrl() {
+  if (typeof window === 'undefined') {
+    return null;
+  }
   return <CopyText text={window.location.href} />;
 }
