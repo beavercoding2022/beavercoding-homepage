2be9d26fc75e6a8d84fc08b12786475d1f243989
orlein <pcj9024@gmail.com>
2024-01-29 16:42:23 +0900

    update url copier

--- src/components/ui/CopyCurrentUrl.tsx
+++ src/components/ui/CopyCurrentUrl.tsx
@@ -7,7 +7,7 @@ export default function CopyCurrentUrl() {
   const [url, setUrl] = React.useState('');
 
   React.useEffect(() => {
-    setUrl(window.location.href);
+    setUrl(decodeURIComponent(window.location.href));
   }, []);
 
   return <CopyText text={url} />;
