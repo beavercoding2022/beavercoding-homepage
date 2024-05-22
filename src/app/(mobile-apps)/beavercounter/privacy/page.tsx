3718e232a6866bdff67c65d93d710cc1dc184fa8
orlein <pcj9024@gmail.com>
2024-05-22 22:02:12 +0900

    add korean version

--- src/app/(mobile-apps)/beavercounter/privacy/page.tsx
+++ src/app/(mobile-apps)/beavercounter/privacy/page.tsx
@@ -3,6 +3,9 @@ export default async function BeavercounterPrivacyPage() {
     <div className="flex flex-col">
       <h1 className="text-3xl font-bold">Privacy Policy</h1>
       <p>This app does not store, transmit, or process any personal information.</p>
+
+      <h1>개인정보 처리방침</h1>
+      <p>이 앱은 개인정보를 저장, 전송, 처리하지 않습니다.</p>
     </div>
   );
 }
