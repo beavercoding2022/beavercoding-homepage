e0297b0d09c88474841d56f53ef742bc80d1bde8
orlein <pcj9024@gmail.com>
2024-05-22 22:01:52 +0900

    create beavercounter privacy page

---
+++ src/app/(mobile-apps)/beavercounter/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default async function BeavercounterPrivacyPage() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-3xl font-bold">Privacy Policy</h1>
+      <p>This app does not store, transmit, or process any personal information.</p>
+    </div>
+  );
+}
