94bcad35e520eb5875efcded32c9b7c20a1ecbbc
orlein <pcj9024@gmail.com>
2024-12-01 11:07:10 +0900

    update pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default async function Index() {
   return (
     <header className="flex-1 flex flex-col gap-6">
       <div className="flex flex-col min-h-[calc(100vh-4rem)] gap-16 items-center justify-center animate-in">
-        <h1 className="sr-only">Park Changjun Homepage</h1>
+        <h1 className="sr-only">BeaverCoding Homepage</h1>
         <p className="text-3xl lg:text-4xl !leading-tight mx-auto text-center break-keep">
-          Park Changjun <br />
+          BeaverCoding <br />
           <span className="font-bold">A Full-Stack Web Developer</span>
           <br /> for your business.
         </p>
