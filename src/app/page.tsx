2d15cb47548fac3a63b8849267b3f1f69153bd90
orlein <pcj9024@gmail.com>
2024-12-02 20:30:37 +0900

    update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { HoverEffect } from '@/components/ui/card-hover-effect';
 import { getValidCategories } from '@/src/backend/posts';
+import Image from 'next/image';
 
 export default async function Index() {
   const [categories] = await Promise.all([getValidCategories()]);
 
   return (
     <header className="flex-1 flex flex-col gap-6">
-      <div className="flex flex-col min-h-[calc(100vh-4rem)] gap-16 items-center justify-center animate-in">
+      <div className="flex flex-col min-h-screen gap-16 items-center justify-center animate-in">
         <h1 className="sr-only">BeaverCoding Homepage</h1>
+        <Image src={'/logo.png'} alt="logo" width={200} height={200} />
         <p className="text-3xl lg:text-4xl !leading-tight mx-auto text-center break-keep">
           BeaverCoding <br />
           <span className="font-bold">A Full-Stack Web Developer</span>
